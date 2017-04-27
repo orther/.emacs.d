aff1a2bf6b9dc6f93c48477097f0504c4b69ae18
Bryan Gilbert <gilbertw1@gmail.com>
2017-04-27 11:50:14 -0400

    merge

--- core/core-popups.el
+++ core/core-popups.el
@@ -172,12 +172,8 @@ for :align t on every rule."
                (doom-hide-modeline-mode +1))
               ((and (symbolp modeline)
                     (not (eq modeline 't)))
-<<<<<<< HEAD
-               (let ((doom--mode-line (doom-modeline modeline)))
-=======
                (setq-local doom--modeline-format (doom-modeline modeline))
                (when doom--modeline-format
->>>>>>> b2d19eac... Fix disappearing modeline due to loss of state
                  (doom-hide-modeline-mode +1)))))
     ;; show modeline
     (when doom-hide-modeline-mode
