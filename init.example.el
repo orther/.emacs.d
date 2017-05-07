7e85ff5f16a5bd2b4340943e8f244f850b250c76
Henrik Lissner <henrik@lissner.net>
2017-05-07 15:10:46 +0200

    Add tools/gist

--- init.example.el
+++ init.example.el
@@ -30,9 +30,14 @@
        window
       
        :tools
-       eshell
-       dired
-       restclient
+       dired           ; making dired pretty [functional]
+       electric-indent ; smarter, keyword-based electric-indent
+       eshell          ; a consistent, cross-platform shell (WIP)
+       gist            ; manage & create gists
+       macos           ; macos-specific commands
+       rotate-text     ; cycle region at point between text candidates
+       tmux            ; an API for interacting with tmux
+       upload          ; map local to remote projects via ssh/ftp
 
        :lang
        assembly
