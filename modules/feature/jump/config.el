8b0c03d38a26319947e05d15365e3b74187e99ff
Henrik Lissner <henrik@lissner.net>
2017-05-10 08:41:13 +0200

    feature/jump: recenter after counsel-grep-post-action-hook

--- modules/feature/jump/config.el
+++ modules/feature/jump/config.el
@@ -13,3 +13,44 @@
     ("DevDocs.io"    . "http://devdocs.io/#q=%s")
     ("StackOverflow" . "https://stackoverflow.com/search?q=%s"))
   "An alist that maps online resources to their search url.")
+
+(set! :popup "*xref*" :size 10 :noselect t :autokill t :autoclose t)
+
+;; Let me control what backends to fall back on
+(setq-default xref-backend-functions '())
+
+(def-setting! :xref-backend (mode function)
+  "TODO"
+  `(add-hook! ,mode
+     (add-hook 'xref-backend-functions #',function nil t)))
+
+;; Recenter after certain jumps
+(add-hook!
+  '(imenu-after-jump-hook evil-jumps-post-jump-hook counsel-grep-post-action-hook)
+  'recenter)
+
+
+;;
+;; Packages
+;;
+
+(def-package! dumb-jump
+  :commands (dumb-jump-go dumb-jump-quick-look dumb-jump-back)
+  :config
+  (setq dumb-jump-default-project doom-emacs-dir))
+
+
+;; (def-package! ggtags
+;;   :commands (ggtags-find-tag-dwim
+;;              ggtags-find-tag-mouse
+;;              ggtags-find-definition
+;;              ggtags-find-reference
+;;              ggtags-find-other-symbol
+;;              ggtags-find-tag-regexp
+;;              ggtags-idutils-query
+;;              ggtags-grep
+;;              ggtags-find-file
+;;              ggtags-query-replace
+;;              ggtags-delete-tags
+;;              ggtags-explain-tags))
+
