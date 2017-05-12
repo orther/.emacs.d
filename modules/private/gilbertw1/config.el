f596e761c91d669a19feb336e701e829e3381cfa
Bryan Gilbert <gilbertw1@gmail.com>
2017-05-12 12:57:05 -0400

    widen fringes to easily distinguish git-gutter changes from window border

--- modules/private/gilbertw1/config.el
+++ modules/private/gilbertw1/config.el
@@ -9,3 +9,6 @@
 ;; projectile ignore directories
 (setq projectile-globally-ignored-directories '("target" ".ensime_cache" ".fingerprint" "project/target"))
 (setq grep-find-ignored-directories '("target" ".ensime_cache" ".fingerprint"))
+
+;; Widen fringes (easier to see git-gutter with twm window border)
+(fringe-mode '(12 . 12))
