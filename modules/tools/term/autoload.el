8b3e1db3d1402b38af9acff17629062a3088e41a
Henrik Lissner <henrik@lissner.net>
2017-05-15 14:08:04 +0200

    tools/term: refactor + better term popup support

--- modules/tools/term/autoload.el
+++ modules/tools/term/autoload.el
@@ -1,29 +1,51 @@
 ;;; tools/term/autoload.el
 
 ;;;###autoload
-(defun +term ()
-  (interactive)
-  (call-interactively 'multi-term))
+(defun +term (&optional project-root)
+  "Open a terminal buffer in the current window. If PROJECT-ROOT (C-u) is
+non-nil, cd into the current project's root."
+  (interactive "P")
+  (let ((default-directory (if project-root (doom-project-root) default-directory)))
+    (call-interactively 'multi-term)))
 
 ;; BMACS - term popup modeled after scratch buffer popup
 ;;;###autoload
-(defun +term/popup ()
-  (interactive)
+(defun +term/popup (&optional project-root)
+  "Open a terminal popup window. If PROJECT-ROOT (C-u) is non-nil, cd into the
+current project's root."
+  (interactive "P")
   (require 'multi-term)
-  (let ((buf (get-buffer "*doom-esc:term*")))
-    (if buf
-      (doom-popup-buffer buf)
-      (let* ((buffer (multi-term-get-buffer current-prefix-arg))
-             (window (doom-popup-buffer buffer :popup t :align t :size 25 :select t)))
-        (select-window window)
-        (setq multi-term-buffer-list (nconc multi-term-buffer-list (list buffer)))
-        (multi-term-internal)
-        (with-current-buffer buffer
-          (rename-buffer "*doom-esc:term*"))))))
+  (let* ((default-directory (if project-root (doom-project-root) default-directory))
+         (buffer (multi-term-get-buffer current-prefix-arg))
+         (window (doom-popup-buffer buffer)))
+    (select-window window)
+    (setq multi-term-buffer-list (nconc multi-term-buffer-list (list buffer)))
+    (multi-term-internal)))
 
-;; BMACS - term popup in project root
 ;;;###autoload
-(defun +term/project-popup ()
+(defun +term/popup-in-project ()
+  "Open a terminal popup window in the root of the current project."
   (interactive)
-  (let ((default-directory (projectile-project-root)))
-    (call-interactively '+term/popup)))
+  (+term/popup t))
+
+
+
+;<<<<<<< HEAD
+;  (let ((buf (get-buffer "*doom-esc:term*")))
+;    (if buf
+;      (doom-popup-buffer buf)
+;      (let* ((buffer (multi-term-get-buffer current-prefix-arg))
+;             (window (doom-popup-buffer buffer :popup t :align t :size 25 :select t)))
+;        (select-window window)
+;        (setq multi-term-buffer-list (nconc multi-term-buffer-list (list buffer)))
+;        (multi-term-internal)
+;        (with-current-buffer buffer
+;          (rename-buffer "*doom-esc:term*"))))))
+;
+;;; BMACS - term popup in project root
+;;;;###autoload
+;(defun +term/project-popup ()
+;  (interactive)
+;  (let ((default-directory (projectile-project-root)))
+;    (call-interactively '+term/popup)))
+;=======
