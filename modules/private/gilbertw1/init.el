21728209a5a3fe01ec2c9db5f03d4aa4967dff7a
Bryan Gilbert <gilbertw1@gmail.com>
2017-06-15 09:14:51 -0400

    added private init.el

---
+++ modules/private/gilbertw1/init.el
@@ -0,0 +1,7 @@
+;;; private/gilbertw1/init.el -*- lexical-binding: t; -*-
+
+(setq +doom-modeline-height 25
+      +doom-font (font-spec :family "Iosevka" :size 24)
+      +doom-variable-pitch-font (font-spec :family "Iosevka" :size 25)
+      +doom-unicode-font (font-spec :family "Iosevka" :size 24)
+      nlinum-format "%3d ")
