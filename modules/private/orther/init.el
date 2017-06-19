cea6d392c5540061be9a0913d4c872b0d89a7c2d
Brandon Orther <brandon@omt.tech>
2017-06-19 03:10:17 -0700

    Switch to use the font Iosevka w/ weight ultralight

--- modules/private/orther/init.el
+++ modules/private/orther/init.el
@@ -9,9 +9,9 @@
 
       +doom-modeline-height 25
 
-      +doom-font (font-spec :family "Iosevka" :size 12)
-      ;; +doom-variable-pitch-font (font-spec :family "Iosevka" :size 13)
-      +doom-unicode-font (font-spec :family "Iosevka" :size 12)
+      +doom-font (font-spec :family "Iosevka" :size 12 :weight 'ultralight)
+      +doom-variable-pitch-font (font-spec :family "Iosevka" :size 12 :weight 'ultralight)
+      +doom-unicode-font (font-spec :family "Iosevka" :size 12 :weight 'ultralight)
       ;; +doom-font (font-spec :family "SF Mono" :size 12)
-      +doom-variable-pitch-font (font-spec :family "SF Mono" :size 13)
+      ;; +doom-variable-pitch-font (font-spec :family "SF Mono" :size 13)
       )
