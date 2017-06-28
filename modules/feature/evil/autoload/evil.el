a0ec59f3181508af6ee650baa12d1264790dfc0c
Bryan Gilbert <gilbertw1@gmail.com>
2017-06-28 11:21:15 -0400

    remove debug message from custom evil command

--- modules/feature/evil/autoload/evil.el
+++ modules/feature/evil/autoload/evil.el
@@ -41,7 +41,6 @@
 (evil-define-operator evil-delete-without-register-if-whitespace (beg end type reg yank-handler)
   (interactive "<R><y>")
   (let ((text (replace-regexp-in-string "\n" "" (filter-buffer-substring beg end))))
-    (message text)
     (if (string-match-p "^\\s-*$" text)
       (evil-delete beg end type ?_)
       (evil-delete beg end type reg yank-handler))))
