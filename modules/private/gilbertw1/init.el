054ea9d6bc8733308b249a1a4817f30d7b61ff9e
Bryan Gilbert <gilbertw1@gmail.com>
2017-06-30 17:06:50 -0400

    updated font ui variables in init.el

--- modules/private/gilbertw1/init.el
+++ modules/private/gilbertw1/init.el
@@ -1,9 +1,9 @@
 ;;; private/gilbertw1/init.el -*- lexical-binding: t; -*-
 
-(setq +doom-modeline-height 25
-      +doom-font (font-spec :family "Iosevka" :size 24)
-      +doom-variable-pitch-font (font-spec :family "Iosevka" :size 25)
-      +doom-unicode-font (font-spec :family "Iosevka" :size 24)
+(setq doom-modeline-height 25
+      doom-font (font-spec :family "Iosevka" :size 25)
+      doom-variable-pitch-font (font-spec :family "Iosevka" :size 25)
+      doom-unicode-font (font-spec :family "Iosevka" :size 24)
       doom-line-number-lpad 3)
 
 ;; increasing elisp runtime thresholds
